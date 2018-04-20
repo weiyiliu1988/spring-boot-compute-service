23df6921cc783b2eb3c215fd882570dc0f3efd49
liuwy <731839038@qq.com>
2018-04-20 19:05:30 +0800

    update

--- src/main/java/cn/com/studyshop/controller/ComputeController.java
+++ src/main/java/cn/com/studyshop/controller/ComputeController.java
@@ -1,5 +1,7 @@
 package cn.com.studyshop.controller;
 
+import java.net.MalformedURLException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,20 @@ public class ComputeController {
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
 		client.getServices().forEach(o -> System.out.println(o));
+
+		// return discoveryClient.getInstances(s).get(0).getUri().toString();
+
+		try {
+			System.out.println(client.getInstances("compute-service").get(0).getUri().toURL().toString());
+
+			logger.debug("service url:->{}", client.getInstances("compute-service").get(0).getUri().toURL());
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// client.getInstances(serviceId)
 		Integer r = a + b;
 		logger.info("/add  result:{}", r);
 		return r;
